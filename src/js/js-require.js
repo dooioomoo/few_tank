092f96a18d1b5d21c660f9b5d23fbae948330d9d
dooioomoo <dooioomoo@gmail.com>
2020-12-28 17:10:43 +0900

    Update js-require.js

--- src/js/js-require.js
+++ src/js/js-require.js
@@ -2,8 +2,8 @@ const vo = "vendor/";
 //基本环境的js文件，此处列表会加载在所有js之前
 module.exports = {
     list: [
-        vo + "components/jquery/jquery.js",
-        vo + "components/jqueryui/jquery-ui.js",
+        vo + "node_modules/jquery/dist/jquery.js",
+        vo + "node_modules/jqueryui/jquery-ui.js",
         vo + "twitter/bootstrap/dist/js/bootstrap.bundle.js",
     ],
 };
